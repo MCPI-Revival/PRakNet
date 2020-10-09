7bb4f2aaa03e56db3845be3427a587adaea0b378
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-10-10 00:54:05 +0300

    Update handler.py

--- praknet/handler.py
+++ praknet/handler.py
@@ -6,4 +6,4 @@ def handle_unconnected_ping(data):
     packets.unconnected_pong["server_guid"] = 12345678
     packets.unconnected_pong["magic"] = packets.unconnected_ping["magic"]
     packets.unconnected_pong["data"] = "MCPE;Dedicated Server;390;1.14.60;0;10;13253860892328930865;Bedrock level;Survival;1;19132;19133;"
-    data = packet.write_unconnected_pong()
+    data = packets.write_unconnected_pong()
