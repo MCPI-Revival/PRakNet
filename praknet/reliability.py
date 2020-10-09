ca25cff2943868e0303d7c07d6c503b13f3d8edf
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-10-09 07:52:08 +0300

    Create reliability.py

---
+++ praknet/reliability.py
@@ -0,0 +1,8 @@
+unreliable = 0
+unreliable_sequenced = 1
+reliable = 2
+reliable_ordered = 3
+reliable_sequenced = 4
+unreliable_ack_receipt = 5
+reliable_ack_receipt = 6
+reliable_ordered_ack_receipt = 7
