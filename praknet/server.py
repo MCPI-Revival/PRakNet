0b315027a268997361fb80ffea9461b884c6eee9
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-10-10 00:08:51 +0300

    Update server.py

--- praknet/server.py
+++ praknet/server.py
@@ -2,6 +2,13 @@ from praknet import handler
 from praknet import messages
 from praknet import socket
 
+options = {
+    "name": "",
+    "port": 0,
+    "custom_handler": lambda data, addr, socket: 0,
+    "custom_packets": [0x84]
+}
+
 @staticmethod
 def packet_handler(data, address):
     if data[0] == messages.ID_UNCONNECTED_PING:
