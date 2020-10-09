d86a0de5b34673b4e93dff7afc9f6a085e5a53d0
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-10-10 00:13:11 +0300

    Update server.py

--- praknet/server.py
+++ praknet/server.py
@@ -9,12 +9,10 @@ options = {
     "custom_packets": [0x84]
 }
 
-@staticmethod
 def packet_handler(data, address):
     if data[0] == messages.ID_UNCONNECTED_PING:
         socket.sendBuffer(handler.handle_unconnected_ping(data), address)
 
-@staticmethod
 def run():
     socket.create_socket(19132)
     while True:
