c7f61628d4529b13b819b233bf191a20d75a2555
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-10-09 08:00:31 +0300

    Update reliability.py

--- praknet/reliability.py
+++ praknet/reliability.py
@@ -6,3 +6,49 @@ reliable_sequenced = 4
 unreliable_ack_receipt = 5
 reliable_ack_receipt = 6
 reliable_ordered_ack_receipt = 7
+
+@staticmethod
+def is_reliable(reliability):
+    if reliability == reliable:
+        return True
+    elif reliability == reliable_ordered:
+        return True
+    elif reliability == reliable_sequenced:
+        return True
+    elif reliability == reliable_ack_receipt:
+        return True
+    elif reliability == reliable_ordered_ack_receipt:
+        return True
+    else:
+        return False
+        
+@staticmethod
+def isSequenced(reliability):
+        if reliability == unreliable_sequenced:
+            return True
+        elif reliability == reliable_sequenced:
+            return True
+        else:
+            return False
+
+@staticmethod
+def is_ordered(reliability):
+    if reliability == reliable_ordered:
+        return True
+    elif reliability == reliable_ordered_ack_receipt:
+        return True
+    else:
+        return False
+
+@staticmethod
+def is_sequenced_or_ordered(reliability):
+    if reliability == self.unreliable_sequenced:
+        return True
+    elif reliability == self.reliable_ordered:
+        return True
+    elif reliability == self.reliable_sequenced:
+        return True
+    elif reliability == self.reliable_ordered_ack_receipt:
+        return True
+    else:
+        return False
