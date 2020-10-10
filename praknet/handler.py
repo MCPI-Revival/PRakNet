591408c22f907a742fdc6cc7a668351d0da0a135
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-10-10 19:24:28 +0300

    Update handler.py

--- praknet/handler.py
+++ praknet/handler.py
@@ -4,7 +4,7 @@ from praknet import server
 def handle_unconnected_ping(data):
     packets.read_unconnected_ping(data)
     packets.unconnected_pong["time"] = packets.unconnected_ping["time"]
-    packets.unconnected_pong["server_guid"] = 12345678
+    packets.unconnected_pong["server_guid"] = server.options["server_guid"]
     packets.unconnected_pong["magic"] = packets.unconnected_ping["magic"]
     packets.unconnected_pong["data"] = server.options["name"]
     return packets.write_unconnected_pong()
