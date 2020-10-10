75e2f7150c590208148fdd6df44569e8462e115f
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-10-10 21:46:33 +0300

    Update packets.py

--- praknet/packets.py
+++ praknet/packets.py
@@ -210,7 +210,13 @@ def read_open_connection_request_1(data):
     open_connection_request_1["id"] = data[0]
     open_connection_request_1["magic"] = data[1:1 + 16]
     open_connection_request_1["protocol_version"] = struct.unpack(">B", data[17:17 + 1])[0]
-    open_connection_request_1["mtu_size"] = struct.unpack(">H", data[18:18 + 2])
+    open_connection_request_1["mtu_size"] = len(data)
+    
+def write_open_connection_request_1():
+    buffer = b""
+    buffer += struct.pack(">B", open_connection_request_1["id"])
+    buffer += struct.pack(">B", open_connection_request_1["protocol_version"])
+    buffer += b"" 
 
 def read_nack(data):
     nack["id"] = data[0]
