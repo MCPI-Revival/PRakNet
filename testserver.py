f7cdb79017bcf568c3d25f8107e1cef78c8714cf
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-10-31 23:44:03 +0200

    Update testserver.py

--- testserver.py
+++ testserver.py
@@ -1,4 +1,36 @@
-from praknet import server
+from praknet import packets
+from praknet
+import server
+import struct
 
+entities = 0
+
+def encode_pos(pos):
+    return struct.pack("!f", 128 + pos[0]) + struct.pack("!f", 64 + pos[1]) + struct.pack("!f", 128 + pos[2])
+
+def decode_pos(pos):
+    return [struct.unpack("!f", pos[:4])[0] - 128, struct.unpack("!f", pos[4:8])[0] - 64, struct.unpack("!f", pos[8:12])[0] - 128]
+
+def send_encapsulated(data, address, encapsulation, iteration):
+    packets.encapsulated["data_packet"] = data
+    packets.encapsulated["encapsulation"] = encapsulation
+    packets.encapsulated["iteration"] = iteration
+    packet = packets.write_encapsulated()
+    server.socket.send_buffer(packet, address)
+    server.add_to_queue(packet, address)
+
+def custom_handler(data, address):
+    connection = server.get_connection(address[0], address[1])
+    packets.read_encapsulated(data)
+    packet = packets.encapsulated
+    id = packet["data_packet"][0]
+    if id == 0x82:
+        new_packet = b"\x83\x00\x00\x00\x00"
+        send_encapsulated(new_packet, address, 0x60, connection["iteration"])
+        entities += 1
+        new_packet = b"\x87\x01\x02\x03\x04\x00\x00\x00\x00\x00\x00\x00\x01" + struct.pack(">I", entities) + encode_pos([0, 4, 0])
+        send_encapsulated(new_packet, address, 0x60, connection["iteration"])
+
+server.set_option("custom_handler", custom_handler)
 server.set_option("name", "MCCPP;Demo;PRakNet Server")
 server.run()
