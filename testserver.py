c54328ac94a662fd72105a102784eeb6dafe2b49
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-05 19:49:28 +0200

    Update testserver.py

--- testserver.py
+++ testserver.py
@@ -14,14 +14,14 @@ def custom_handler(data, address):
     connection = server.get_connection(address[0], address[1])
     packets.read_encapsulated(data)
     packet = packets.encapsulated
-    id = packet["data_packet"][0]
+    id = packet["body"][0]
     if id == 0x82:
         new_packet = b"\x83\x00\x00\x00\x00"
-        server.send_encapsulated(new_packet, address, 0x60, connection["iteration"])
+        server.send_encapsulated(new_packet, address, 3, connection["sequence_order"])
         print(f"LOGIN STATUS -> {new_packet}")
         server.options["entities"] += 1
         new_packet = b"\x87\x01\x02\x03\x04\x00\x00\x00\x00\x00\x00\x00\x01" + struct.pack(">I", server.options["entities"]) + encode_pos([0, 4, 0])
-        server.send_encapsulated(new_packet, address, 0x60, connection["iteration"])
+        server.send_encapsulated(new_packet, address, 3, connection["sequence_order"])
         print(f"START GAME -> {new_packet}")
 
 server.set_option("custom_handler", custom_handler)
