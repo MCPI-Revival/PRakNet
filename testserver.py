3c0cf2929bd4f8e2016fd3ad9dc56dceee1c0bad
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-05 21:55:19 +0200

    Update testserver.py

--- testserver.py
+++ testserver.py
@@ -21,7 +21,7 @@ def custom_handler(data, address):
         print(f"LOGIN STATUS -> {new_packet}")
         server.options["entities"] += 1
         new_packet = b"\x87\x01\x02\x03\x04\x00\x00\x00\x00\x00\x00\x00\x01" + struct.pack(">I", server.options["entities"]) + encode_pos([0, 4, 0])
-        server.send_encapsulated(new_packet, address, 3, connection["sequence_order"], True)
+        server.send_encapsulated(new_packet, address, 3, connection["sequence_order"])
         print(f"START GAME -> {new_packet}")
 
 server.set_option("custom_handler", custom_handler)
