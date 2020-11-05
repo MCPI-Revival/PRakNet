aa0e1de24399131942864ee2b0b36ec13128f985
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-05 19:47:40 +0200

    Update server.py

--- praknet/server.py
+++ praknet/server.py
@@ -31,7 +31,7 @@ def add_connection(addr, port):
         "address": (addr, port, 4),
         "connecton_state": status["connecting"],
         "packets_queue": [],
-        "iteration": 0
+        "sequence_order": 0
     }
 
 def remove_connection(addr, port):
@@ -52,10 +52,10 @@ def set_option(option, value):
 def add_to_queue(data, address):
     connection = get_connection(address[0], address[1])
     connection["packets_queue"].append(data)
-    if connection["iteration"] >= 255:
-        connection["iteration"] = 0
+    if connection["sequence_order"] >= 255:
+        connection["sequence_order"] = 0
     else:
-        connection["iteration"] += 1
+        connection["sequence_order"] += 1
 
 def get_last_packet(address):
     connection = get_connection(address[0], address[1])
@@ -65,14 +65,17 @@ def get_last_packet(address):
     else:
         return b""
     
-def send_encapsulated(data, address, reliability, sequence_order, reliable_frame_index = 0, sequenced_frame_index = 0, ordered_frame_index = 0, order_channel = 0):
+def send_encapsulated(data, address, reliability, sequence_order, reliable_frame_index = 0, sequenced_frame_index = 0, ordered_frame_index = 0, order_channel = 0, compound_size = 0, compound_id = 0, compound_index = 0):
     packets.encapsulated["body"] = data
     packets.encapsulated["flags"] = reliability
     packets.encapsulated["sequence_order"] = sequence_order
     packets.encapsulated["reliable_frame_index"] = reliable_frame_index
     packets.encapsulated["sequenced_frame_index"] = sequenced_frame_index
-    packets.encapsulated["ordered_frame_index"] = ordered_frame_index
-    packets.encapsulated["order_channel"] = order_channel
+    packets.encapsulated["order"]["ordered_frame_index"] = ordered_frame_index
+    packets.encapsulated["order"]["order_channel"] = order_channel
+    packets.encapsulated["fragment"]["compound_size"] = compound_size
+    packets.encapsulated["fragment"]["compound_id"] = compound_id
+    packets.encapsulated["fragment"]["index"] = compound_index
     packet = packets.write_encapsulated()
     socket.send_buffer(packet, address)
     add_to_queue(packet, address)
@@ -83,7 +86,7 @@ def packet_handler(data, address):
     if connection != None:
         if id == messages.ID_NACK:
             packets.read_encapsulated(get_last_packet(address))
-            packets.encapsulated["sequence_order"] = connection["iteration"]
+            packets.encapsulated["sequence_order"] = connection["sequence_order"]
             socket.send_buffer(packets.write_encapsulated(), address)
         elif id == messages.ID_ACK:
             pass
@@ -95,13 +98,13 @@ def packet_handler(data, address):
             print("DATA_PACKET -> " + str(hex(id)))
             if need_ack:
                 packets.ack["packets"] = []
-                packets.ack["packets"].append(connection["iteration"])
+                packets.ack["packets"].append(connection["sequence_order"])
                 socket.send_buffer(packets.write_ack(), address)
             if id < 0x80:
                 if connection["connecton_state"] == status["connecting"]:
                     if id == messages.ID_CONNECTION_REQUEST:
                         buffer = handler.handle_connection_request(data_packet, connection)
-                        send_encapsulated(buffer, address, 0x00, connection["iteration"])
+                        send_encapsulated(buffer, address, 0, connection["sequence_order"])
                     elif id == messages.ID_NEW_CONNECTION:
                         connection["connecton_state"] = status["connected"]
                 elif id == messages.ID_CONNECTION_CLOSED:
@@ -110,7 +113,7 @@ def packet_handler(data, address):
                     connection["connecton_state"] = status["disconnected"]
                 elif id == messages.ID_CONNECTED_PING:
                     buffer = handler.handle_connected_ping(data_packet)
-                    send_encapsulated(buffer, address, 0x00, connection["iteration"])
+                    send_encapsulated(buffer, address, 0, connection["sequence_order"])
             if connection["connecton_state"] == status["connected"]:
                 options["custom_handler"](data, address)
     elif id == messages.ID_UNCONNECTED_PING:
