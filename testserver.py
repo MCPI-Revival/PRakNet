69b6112b306f787e9f5680ba4adb723734cb65b5
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-05 22:56:19 +0200

    Update testserver.py

--- testserver.py
+++ testserver.py
@@ -3,10 +3,10 @@ from praknet import server
 import struct
 
 def encode_pos(pos):
-    return struct.pack("!f", 128 + pos[0]) + struct.pack("!f", 64 + pos[1]) + struct.pack("!f", 128 + pos[2])
+    return struct.pack(">f", 128 + pos[0]) + struct.pack(">f", 64 + pos[1]) + struct.pack(">f", 128 + pos[2])
 
 def decode_pos(pos):
-    return [struct.unpack("!f", pos[:4])[0] - 128, struct.unpack("!f", pos[4:8])[0] - 64, struct.unpack("!f", pos[8:12])[0] - 128]
+    return [struct.unpack(">f", pos[:4])[0] - 128, struct.unpack(">f", pos[4:8])[0] - 64, struct.unpack(">f", pos[8:12])[0] - 128]
 
 def custom_handler(data, address):
     if not "entities" in server.options:
@@ -20,7 +20,7 @@ def custom_handler(data, address):
         server.send_encapsulated(new_packet, address, 3, connection["sequence_order"], True)
         print(f"LOGIN STATUS -> {new_packet}")
         server.options["entities"] += 1
-        new_packet = b"\x87\x01\x02\x03\x04\x00\x00\x00\x00\x00\x00\x00\x01" + struct.pack(">I", server.options["entities"]) + encode_pos([0, 4, 0])
+        new_packet = b"\x87\x01\x02\x03\x04\x00\x00\x00\x00\x00\x00\x00\x01" + struct.pack(">L", server.options["entities"]) + encode_pos([0, 4, 0])
         server.send_encapsulated(new_packet, address, 3, connection["sequence_order"])
         print(f"START GAME -> {new_packet}")
 
