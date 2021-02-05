f5c7c0bee8b1b28cba8bf44a2c7806d718939c44
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-02-05 19:21:52 +0200

    Update testserver.py

--- testserver.py
+++ testserver.py
@@ -19,14 +19,14 @@ def custom_handler(data, address):
         length = struct.unpack(">H", packet["body"][1:1 + 2])[0]
         connection["username"] = packet["body"][3:3 + length].decode()
         new_packet = b"\x83\x00\x00\x00\x00"
-        server.send_encapsulated(new_packet, address, 0, connection["sequence_order"], True)
+        server.send_encapsulated(new_packet, address, 0, connection["sequence_order"])
         server.options["entities"] += 1
         connection["entity_id"] = server.options["entities"]
         new_packet = b"\x87\x01\x02\x03\x04\x00\x00\x00\x00\x00\x00\x00\x01" + struct.pack(">l", server.options["entities"]) + encode_pos([0, 4, 0])
         connection["pos"] = [0, 4, 0]
         connection["yaw"] = 0
         connection["pitch"] = 0
-        server.send_encapsulated(new_packet, address, 0, connection["sequence_order"], True)
+        server.send_encapsulated(new_packet, address, 0, connection["sequence_order"])
     elif id == 0x94:
         server.send_ack_queue(address)
         connection["pos"] = decode_pos(packet["body"][5:5 + 12])
@@ -34,12 +34,12 @@ def custom_handler(data, address):
         connection["pitch"] = struct.unpack(">f", packet["body"][21:21 + 4])[0]
         message = f"X: {connection['pos'][0]} Y: {connection['pos'][1]} Z: {connection['pos'][2]} YAW: {connection['yaw']} PITCH: connection['pitch']"
         new_packet = b"\x85" + struct.pack(">H", len(message)) + message.encode()
-        server.send_encapsulated(new_packet, address, 0, connection["sequence_order"], True)
+        server.send_encapsulated(new_packet, address, 0, connection["sequence_order"])
     elif id == 0x84:
         server.send_ack_queue(address)
         message = f"{connection['username']} joined the game."
         new_packet = b"\x85" + struct.pack(">H", len(message)) + message.encode()
-        server.broadcast_encapsulated(new_packet, 0, connection["sequence_order"], True)
+        server.broadcast_encapsulated(new_packet, 0, connection["sequence_order"])
         print(message)
 
 server.set_option("custom_handler", custom_handler)
