6464148b8b96d09c344d2d337d8179a7fed0efaa
MFDGaming <alexandros.argentakis@gmail.com>
2021-02-05 16:22:20 +0200

    idk

--- testserver.py
+++ testserver.py
@@ -16,22 +16,32 @@ def custom_handler(data, address):
     packet = packets.encapsulated
     id = packet["body"][0]
     if id == 0x82:
-        length = struct.unpack(">H", data[1:1 + 2])[0]
-        connection["username"] = data[3:3 + length].decode()
-        connection["client_id"] = struct.unpack(">q", data[11 + length:])[0]
+        length = struct.unpack(">H", packet["body"][1:1 + 2])[0]
+        connection["username"] = packet["body"][3:3 + length].decode()
         new_packet = b"\x83\x00\x00\x00\x00"
         server.send_encapsulated(new_packet, address, 0, connection["sequence_order"], True)
         server.options["entities"] += 1
+        connection["entity_id"] = server.options["entities"]
         new_packet = b"\x87\x01\x02\x03\x04\x00\x00\x00\x00\x00\x00\x00\x01" + struct.pack(">l", server.options["entities"]) + encode_pos([0, 4, 0])
+        connection["pos"] = [0, 4, 0]
+        connection["yaw"] = 0
+        connection["pitch"] = 0
         server.send_encapsulated(new_packet, address, 0, connection["sequence_order"], True)
     elif id == 0x94:
         server.send_ack_queue(address)
+        connection["pos"] = decode_pos(packet["body"][5:5 + 12])
+        connection["yaw"] = struct.unpack(">f", packet["body"][17:17 + 4])[0]
+        connection["pitch"] = struct.unpack(">f", packet["body"][21:21 + 4])[0]
+        message = f"X: {connection['pos'][0]} Y: {connection['pos'][1]} Z: {connection['pos'][2]} YAW: {connection['yaw']} PITCH: connection['pitch']"
+        new_packet = b"\x85" + struct.pack(">H", len(message)) + message.encode()
+        server.send_encapsulated(new_packet, address, 0, connection["sequence_order"], True)
     elif id == 0x84:
         server.send_ack_queue(address)
-        message = f"{connection["username"]} joined the game."
+        message = f"{connection['username']} joined the game."
         new_packet = b"\x85" + struct.pack(">H", len(message)) + message.encode()
         server.broadcast_encapsulated(new_packet, 0, connection["sequence_order"], True)
-        
+        print(message)
+
 server.set_option("custom_handler", custom_handler)
 server.set_option("name", "MCCPP;MINECON;PRakNet Server")
 server.run()
