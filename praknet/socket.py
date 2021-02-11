340e969451d6332b1f9248f626fc34ee6568f9ff
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-02-11 14:01:59 +0200

    Update socket.py

--- praknet/socket.py
+++ praknet/socket.py
@@ -1,25 +1,57 @@
+################################################################################
+#                                                                              #
+#  __  __ _____ ____   ____                 _                                  #
+# |  \/  |  ___|  _ \ / ___| __ _ _ __ ___ (_)_ __   __ _                      #
+# | |\/| | |_  | | | | |  _ / _` | '_ ` _ \| | '_ \ / _` |                     #
+# | |  | |  _| | |_| | |_| | (_| | | | | | | | | | | (_| |                     #
+# |_|  |_|_|   |____/ \____|\__,_|_| |_| |_|_|_| |_|\__, |                     #
+#                                                    |___/                     #
+# Copyright 2021 MFDGaming                                                     #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
+
 import socket as s
 
 socket = s.socket(s.AF_INET, s.SOCK_DGRAM, s.IPPROTO_UDP)
 
-def create_socket(address):
+def create(address):
     try:
         socket.bind(address)
     except s.error as e:
-        print("Failed to bind!")
-        print(str(e))
-    else:
-        socket.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
-        socket.setsockopt(s.SOL_SOCKET, s.SO_BROADCAST, 1)
+        raise Exception("Failed to bind!\n" + str(e))
+    socket.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
+    socket.setsockopt(s.SOL_SOCKET, s.SO_BROADCAST, 1)
        
 def receive_buffer():
     try:
         return socket.recvfrom(65535)
-    except:
-        pass
+    except s.error:
+        return
           
 def send_buffer(buffer, address):
-    return socket.sendto(buffer, address)
+    try:
+        return socket.sendto(buffer, address)
+    except s.error:
+        return
      
-def close_socket():
+def close():
     socket.close()
