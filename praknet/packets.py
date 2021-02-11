37057e8a8467ce1a1aed5921df32b30904e04407
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-02-11 09:08:46 +0200

    Update packets.py

--- praknet/packets.py
+++ praknet/packets.py
@@ -1,6 +1,39 @@
+################################################################################
+#                                                                              #
+#  __  __ _____ ____   ____                 _                                  #
+# |  \/  |  ___|  _ \ / ___| __ _ _ __ ___ (_)_ __   __ _                      #
+# | |\/| | |_  | | | | |  _ / _` | '_ ` _ \| | '_ \ / _` |                     #
+# | |  | |  _| | |_| | |_| | (_| | | | | | | | | | | (_| |                     #
+# |_|  |_|_|   |____/ \____|\__,_|_| |_| |_|_|_| |_|\__, |                     #
+#                                                    |___/                     #
+# Copyright 2021 MFDGaming                                                     #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
+
 from praknet import messages
 from praknet import reliability
 import struct
+
+# Packet templates
 
 connected_ping = {
     "id": messages.ID_CONNECTED_PING,
@@ -137,6 +170,8 @@ encapsulated = {
     "body": None
 }
 
+# Just to speed up the development
+
 def read_address(data):
     addr = ".".join([
         str((~struct.unpack(">B", data[1:1 + 1])[0]) & 0xff),
@@ -156,6 +191,8 @@ def write_address(address):
         data += struct.pack(">B", (~(int(part))) & 0xff)
     data += struct.pack(">H", port)
     return data
+
+# Decode and Encode PAckets
 
 def read_connected_ping(data):
     return {
