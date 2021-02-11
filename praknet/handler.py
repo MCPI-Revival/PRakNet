ffe3b048c05506e6b3fe9a9ef2e51bb8504f60ad
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-02-11 13:19:49 +0200

    Update handler.py

--- praknet/handler.py
+++ praknet/handler.py
@@ -1,3 +1,34 @@
+################################################################################
+#                                                                              #
+#  __  __ _____ ____   ____                 _                                  #
+# |  \/  |  ___|  _ \ / ___| __ _ _ __ ___ (_)_ __   __ _                      #
+# | |\/| | |_  | | | | |  _ / _` | '_ ` _ \| | '_ \ / _` |                     #
+# | |  | |  _| | |_| | |_| | (_| | | | | | | | | | | (_| |                     #
+# |_|  |_|_|   |____/ \____|\__,_|_| |_| |_|_|_| |_|\__, |                     #
+#                                                    |___/                     #
+# Copyright 2021 MFDGaming                                                     #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
+
 from praknet import packets
 from praknet import server
 from time import time
@@ -11,14 +42,6 @@ def handle_unconnected_ping(data):
     new_packet["magic"] = packet["magic"]
     new_packet["data"] = server.options["name"]
     return packets.write_unconnected_pong(new_packet)
-
-def handle_unconnected_ping_open_connections(data):
-    packets.read_unconnected_ping_open_connections(data)
-    packets.unconnected_pong["time"] = packets.unconnected_ping_open_connections["time"]
-    packets.unconnected_pong["server_guid"] = server.options["server_guid"]
-    packets.unconnected_pong["magic"] = packets.unconnected_ping_open_connections["magic"]
-    packets.unconnected_pong["data"] = server.options["name"]
-    return packets.write_unconnected_pong()
 
 def handle_open_connection_request_1(data):
     packets.read_open_connection_request_1(data)
