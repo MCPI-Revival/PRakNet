d03862758a14d5d83968906a4b529a38ce3e6937
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-02-11 14:09:41 +0200

    Update server.py

--- praknet/server.py
+++ praknet/server.py
@@ -1,3 +1,34 @@
+################################################################################
+#                                                                              #
+#  __  __ _____ ____   ____                 _                                  #
+# |  \/  |  ___|  _ \ / ___| __ _ _ __ ___ (_)_ __   __ _                      #
+# | |\/| | |_  | | | | |  _ / _` | '_ ` _ \| | '_ \ / _` |                     #
+# | |  | |  _| | |_| | |_| | (_| | | | | | | | | | | (_| |                     #
+# |_|  |_|_|   |____/ \____|\__,_|_| |_| |_|_|_| |_|\__, |                     #
+#                                                    |___/                     #
+# Copyright 2021 MFDGaming                                                     #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
+
 import os
 from praknet import handler
 from praknet import packets
@@ -13,34 +44,27 @@ options = {
     "accepted_raknet_protocols": [5]
 }
 
-status = {
-    "connecting": 0,
-    "connected": 1,
-}
-
 connections = {}
 
-def add_connection(addr, port):
-    token = str(addr) + ":" + str(port)
+def add_connection(address):
+    token = str(address[0]) + ":" + str(address[1])
     connections[token] = {
         "mtu_size": 0,
-        "address": (addr, port, 4),
-        "connecton_state": status["connecting"],
+        "address": address,
+        "is_connected": False,
         "packets_queue": [],
         "sequence_order": 0
     }
 
-def remove_connection(addr, port):
-    token = str(addr) + ":" + str(port)
+def remove_connection(address):
+    token = str(address[0]) + ":" + str(address[1])
     if token in connections:
         del connections[token]
 
-def get_connection(addr, port):
-    token = str(addr) + ":" + str(port)
+def get_connection(address):
+    token = str(address[0]) + ":" + str(address[1])
     if token in connections:
         return connections[token]
-    else:
-        return None
 
 def set_option(option, value):
     options[option] = value
