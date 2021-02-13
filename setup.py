1e8eca3830d0a64f61ee0a6638756544b33d96f5
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-02-13 22:59:21 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,51 @@
+################################################################################
+#                                                                              #
+#  __  __ _____ ____   ____                 _                                  #
+# |  \/  |  ___|  _ \ / ___| __ _ _ __ ___ (_)_ __   __ _                      #
+# | |\/| | |_  | | | | |  _ / _` | '_ ` _ \| | '_ \ / _` |                     #
+# | |  | |  _| | |_| | |_| | (_| | | | | | | | | | | (_| |                     #
+# |_|  |_|_|   |____/ \____|\__,_|_| |_| |_|_|_| |_|\__, |                     #
+#                                                    |___/                     #
+# Copyright 2021 MFDGaming                                                     #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name="PRakNet",
+    version="1.0",
+    description="A RakNet implementation for python.",
+    long_description="A Minecraft PI Edition oriented RakNet implementation written in python.",
+    url="https://github.com/MCPI-Revival/PRakNet",
+    author="MFDGaming",
+    author_email="alexandros.argentakis@gmail.com",
+    license="MIT License",
+    classifiers= [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries'
+    ],
+    keywords='RakNet implementation for python.',
+    packages=find_packages(where='.')
+)
