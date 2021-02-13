0034fab70232f813415fe4de13e64d4dc96eb761
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-02-13 21:04:04 +0200

    Update server.py

--- praknet/server.py
+++ praknet/server.py
@@ -61,6 +61,11 @@ def add_connection(address):
 def remove_connection(address):
     token = str(address[0]) + ":" + str(address[1])
     if token in connections:
+        body = bytes([connection_closed["id"]])
+        packet = copy(packets.frame)
+        packet["reliability"] = 0
+        packet["body"] = body
+        send_frame(packet, address)
         del connections[token]
 
 def get_connection(address):
