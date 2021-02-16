d6581a96e1adad7d5bcf652dbbca36f7b7ee48eb
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-02-16 02:42:12 +0200

    Update client.py

--- praknet/client.py
+++ praknet/client.py
@@ -40,8 +40,10 @@ options = {
     "ip": "0.0.0.0",
     "port": 19132,
     "guid": struct.unpack(">Q", os.urandom(8))[0],
-    "protocol": 5,
-    "debug": False
+    "protocol_version": 5,
+    "debug": False,
+    "magic": b"\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78",
+    "mtu_size": 512
 }
 
 # State 0: Offline
@@ -64,3 +66,20 @@ def send_frame(packet):
     client_socket.sendto(packets.write_frame_set(new_packet), [options["ip"], options["port"]])
     connection["sent_packets"].append(packet)
     connection["sequence_number"] += 1  
+
+def connect():
+    connection["state"] = 1
+    new_packet = copy(packets.open_connection_request_1)
+    new_packet["magic"] = options["magic"]
+    new_packet["protocol_version"] = options["protocol_version"]
+    new_packet["mtu_size"] = options["mtu_size"]
+    client_socket.sendto(packets.write_open_connection_request_1(new_packet), [options["ip"], options["port"]])
+    client_socket.recv_from(65535)
+    new_packet = copy(packets.open_connection_request_2)
+    new_packet["magic"] = options["magic"]
+    new_packet["server_address"] = [options["ip"], options["port"]]
+    new_packet["mtu_size"] = options["mtu_size"]
+    new_packet["client_guid"] = options["guid"]
+    client_socket.sendto(packets.write_open_connection_request_2(new_packet), [options["ip"], options["port"]])
+    client_socket.recv_from(65535)
+    
